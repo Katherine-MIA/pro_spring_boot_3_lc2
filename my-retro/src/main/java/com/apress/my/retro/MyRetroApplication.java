598e5ae0074013b9d29c8d26b762fc99723ffed4
Katherine-MIA <ioana.iulia.katherine@gmail.com>
2025-08-29 17:54:23 +0300

    Chapter 2 project as module in a functional state; first victory for today

---
+++ my-retro/src/main/java/com/apress/my/retro/MyRetroApplication.java
@@ -0,0 +1,13 @@
+package com.apress.my.retro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRetroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRetroApplication.class, args);
+	}
+
+}
---
+++ my-retro/src/test/java/com/apress/my/retro/MyRetroApplicationTests.java
@@ -0,0 +1,14 @@
+package com.apress.my.retro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+        //Not implemented yet
+	}
+
+}
